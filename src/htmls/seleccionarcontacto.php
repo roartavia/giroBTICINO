cbd0fa2c3b9224d5f22b8eb5eb3bd7f2b8d57a87
Rodolfo Artavia <rodolfo.artavia@rartavia-2856.local>
2017-07-03 13:13:21 -0600

    done

--- src/htmls/seleccionarcontacto.php
+++ src/htmls/seleccionarcontacto.php
@@ -4,19 +4,46 @@
         echo $newLocation;
         exit;
     }
+    $paisCuenta = '';
+    $current_user = wp_get_current_user();
+    $lastName = $current_user->user_lastname;
+    if (strpos($lastName, '-CR-') !== false) {
+        $paisCuenta = 'Costa Rica';
+    } else {
+        $paisCuenta = 'Guatemala';
+    }
 ?>
 <form method="post">
     <h1 class='label'>Seleccione el contacto que desea editar</h1>
     <select name="opcionesCuentas">
         <?php
             global $wpdb;
-            $rows = $wpdb->get_results('select id, nombre from contactos');
+            $rows = $wpdb->get_results("select id, nombre, apellidos from contactos where pais='$paisCuenta' order by nombre");
             $countRows = sizeof($rows);
 
             for ($i = 0; $i < $countRows; $i++) {
-                $nombre = htmlspecialchars($rows[$i]->nombre);
-                $id = htmlspecialchars($rows[$i]->id);
-                echo "<option value='$id'>$nombre</option>";
+                if(isset($rows[$i]->nombre)) {
+                    if(isset($rows[$i]->apellidos)) {
+                        $nombre = htmlspecialchars($rows[$i]->nombre);
+                        $apellidos = htmlspecialchars($rows[$i]->apellidos);
+                        $nombre = $nombre." ".$apellidos;
+                        $id = htmlspecialchars($rows[$i]->id);
+                        echo "<option value='$id'>$nombre</option>";
+                    } else {
+                        $nombre = htmlspecialchars($rows[$i]->nombre);
+                        $id = htmlspecialchars($rows[$i]->id);
+                        echo "<option value='$id'>$nombre</option>";
+                    }
+                } else {
+                    if(isset($rows[$i]->apellidos)) {
+                        $apellidos = htmlspecialchars($rows[$i]->apellidos);
+                        $id = htmlspecialchars($rows[$i]->id);
+                        echo "<option value='$id'>$apellidos</option>";
+                    } else {
+                        $id = htmlspecialchars($rows[$i]->id);
+                        echo "<option value='$id'>Sin nombre</option>";
+                    }
+                }
             }
         ?>
     </select>
--- src/vba/vbaFunctions.vb
+++ src/vba/vbaFunctions.vb
@@ -51,3 +51,80 @@ Sub getInserts()
         outPutSheet.Cells(i, "A").Value = sqlInsertStr
     Next i
 End Sub
+
+
+Sub getInserts()
+    Dim cuentasSheet As Worksheet
+    Set cuentasSheet = ThisWorkbook.Worksheets("contactos gua")
+    Dim outPutSheet As Worksheet
+    Set outPutSheet = Sheets("InsertCuentas")
+    Dim lastRow As Long
+    lastRow = cuentasSheet.Cells(cuentasSheet.Rows.Count, "A").End(xlUp).Row
+    MsgBox lastRow
+    Dim i As Integer
+    Dim nombre As String, tipo As String, subtipo As String, telefono As String
+    Dim sitioWeb As String, paisFisico As String, provincia As String, canton As String
+    Dim zona As String, direccion As String, latitud As String, longitud As String, descripcion As String
+    For i = 2 To lastRow
+
+    nombreCuenta = Cells(i, "A").Value
+    nombreCuenta = Replace(nombreCuenta, "\", "\\")
+    nombreCuenta = Replace(nombreCuenta, "'", "\'")
+
+    nombre = Cells(i, "B").Value
+    nombre = Replace(nombre, "\", "\\")
+    nombre = Replace(nombre, "'", "\'")
+
+    apellidos = Cells(i, "C").Value
+    apellidos = Replace(apellidos, "\", "\\")
+    apellidos = Replace(apellidos, "'", "\'")
+
+    perfilContactoCargo = Cells(i, "D").Value
+    perfilContactoCargo = Replace(perfilContactoCargo, "\", "\\")
+    perfilContactoCargo = Replace(perfilContactoCargo, "'", "\'")
+
+    telefono = Cells(i, "F").Value
+    telefono = Replace(telefono, "\", "\\")
+    telefono = Replace(telefono, "'", "\'")
+
+    correo = Cells(i, "G").Value
+    correo = Replace(correo, "\", "\\")
+    correo = Replace(correo, "'", "\'")
+
+    pais = Cells(i, "H").Value
+    pais = Replace(pais, "\", "\\")
+    pais = Replace(pais, "'", "\'")
+
+    provincia = Cells(i, "I").Value
+    provincia = Replace(provincia, "\", "\\")
+    provincia = Replace(provincia, "'", "\'")
+
+    canton = Cells(i, "J").Value
+    canton = Replace(canton, "\", "\\")
+    canton = Replace(canton, "'", "\'")
+
+    zona = Cells(i, "K").Value
+    zona = Replace(zona, "\", "\\")
+    zona = Replace(zona, "'", "\'")
+
+    direccion = Cells(i, "L").Value
+    direccion = Replace(direccion, "\", "\\")
+    direccion = Replace(direccion, "'", "\'")
+
+    asistente = Cells(i, "M").Value
+    asistente = Replace(asistente, "\", "\\")
+    asistente = Replace(asistente, "'", "\'")
+
+    telefonoAsistente = Cells(i, "N").Value
+    telefonoAsistente = Replace(telefonoAsistente, "\", "\\")
+    telefonoAsistente = Replace(telefonoAsistente, "'", "\'")
+
+    descripcion = Cells(i, "O").Value
+    descripcion = Replace(descripcion, "\", "\\")
+    descripcion = Replace(descripcion, "'", "\'")
+
+        Dim sqlInsertStr as String
+        sqlInsertStr = "INSERT INTO contactos (nombreCuenta,nombre,apellidos,perfilContactoCargo,telefono,correo,pais,provincia,canton,zona,asistente,telefonoAsistente,descripcion) values (" & "'" & nombreCuenta & "'," & "'" & nombre & "'," & "'" & apellidos & "'," & "'" & perfilContactoCargo & "'," & "'" & telefono & "'," & "'" & correo & "'," & "'" & pais & "'," & "'" & provincia & "'," & "'" & canton & "'," & "'" & zona & "'," & "'" & asistente & "'," & "'" & telefonoAsistente & "'," & "'" & descripcion & "');"
+        outPutSheet.Cells(i, "A").Value = sqlInsertStr
+    Next i
+End Sub
